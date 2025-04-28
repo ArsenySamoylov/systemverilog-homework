a7fc114fb9b440becc7d8b21b8a0bd358dc2f312
ArsenySamoylov <samoylov@ispras.ru>
2025-04-28 14:20:17 +0300

    03-07 fixed (thx Dmitriy Matveev)

--- 03_finite_state_machines/03_07_sort_floats_using_fsm/03_07_sort_floats_using_fsm.sv
+++ 03_finite_state_machines/03_07_sort_floats_using_fsm/03_07_sort_floats_using_fsm.sv
@@ -36,83 +36,99 @@ module sort_floats_using_fsm (
     // The FLEN parameter is defined in the "import/preprocessed/cvw/config-shared.vh" file
     // and usually equal to the bit width of the double-precision floating-point number, FP64, 64 bits.
 
-    enum logic [2:0] {
-        IDLE, 
-        LOAD, 
-        COMP1, 
-        COMP2, 
-        COMP3, 
-        DONE
-    } state, next_state;
+    typedef enum logic [2:0] {
+        st_idle    = 3'd0,
+        st_comp1   = 3'd1,
+        st_comp2   = 3'd2,
+        st_comp3   = 3'd3,
+        st_success = 3'd4
+    } state_t;
 
-    always_ff @(posedge clk) 
+    state_t state, next_state;
+    logic [0:2][FLEN - 1:0] temp, next_temp;
+    logic next_err, next_valid_out;
+
+
+    assign busy = (state != st_idle) && (state != st_success);
+
+    always_comb
     begin
-        if (rst) begin
-            state     <= IDLE;
-            valid_out <= 0;
+        next_state = state;
+        next_temp = temp;
+        next_err = err;
+        next_valid_out = 1'b0;
+        f_le_a = '0;
+        f_le_b = '0;
+
+        case (state)
+        st_idle:
+        begin
+            if (valid_in)
+            begin
+                next_state = st_comp1;
+                next_temp = unsorted;
+                next_err = 1'b0;
+            end
+        end
+
+        st_comp1:
+        begin
+            next_state = st_comp2;
+            next_err = err | f_le_err;
+            {f_le_a, f_le_b} = {temp[0], temp[1]};
+            if (~f_le_res) 
+                {next_temp[0], next_temp[1]} = {temp[1], temp[0]};
+        end
+
+        st_comp2:
+        begin
+            next_state = st_comp3;
+            next_err = err | f_le_err;
+            {f_le_a, f_le_b} = {temp[1], temp[2]};
+            if (~f_le_res) 
+                {next_temp[1], next_temp[2]} = {temp[2], temp[1]};
+        end
+
+        st_comp3: 
+        begin
+            next_state = st_success;
+            next_err = err | f_le_err;
+            {f_le_a, f_le_b} = {temp[0], temp[1]};
+            if (~f_le_res) 
+                {next_temp[0], next_temp[1]} = {temp[1], temp[0]};
+        end
+
+        st_success:
+        begin
+            next_valid_out = 1'b1;
+            next_state = st_idle;
+        end
+
+        default:
+        begin
+            next_state = st_idle;
+        end
+        endcase
+    end
+
+    always_ff @(posedge clk)
+    begin
+        if (rst)
+        begin
+            state     <= st_idle;
+            temp      <= '0;
+            sorted    <= '0;
+            err       <= '0;
+            valid_out <= '0;
         end
         else
-            state <= next_state;
+        begin
+            state     <= next_state;
+            temp      <= next_temp;
+            sorted    <= temp;
+            err       <= next_err;
+            valid_out <= next_valid_out;
+        end
     end
 
-    logic err1, err2, err3;
-    assign busy = (state != IDLE) & (state != DONE);
-
-    always_comb begin
-            case (state)
-                IDLE:
-                if (valid_in) 
-                    next_state = LOAD;
-
-                LOAD: begin
-                    sorted     = unsorted;
-                    next_state = COMP1;
-                end
-
-                COMP1: begin
-                    f_le_a = sorted[0];
-                    f_le_b = sorted[1];
-
-                    if (!f_le_res) begin
-                        sorted[0] = sorted[1];
-                        sorted[1] = sorted[0];
-                    end
-
-                    err1       = f_le_err;
-                    next_state = COMP2;
-                end
-
-                COMP2: begin
-                    f_le_a = sorted[1];
-                    f_le_b = sorted[2];
-
-                    if (!f_le_res) begin
-                        sorted[1] = sorted[2];
-                        sorted[2] = sorted[1];
-                    end
-
-                    err2       = f_le_err;
-                    next_state = COMP3;
-                end
-
-                COMP3: begin
-                    f_le_a = sorted[0];
-                    f_le_b = sorted[1];
-
-                    if (!f_le_res) begin
-                        sorted[0] = sorted[1];
-                        sorted[1] = sorted[0];
-                    end
-
-                    err3       = f_le_err;
-                    next_state = DONE;
-                end
-
-                DONE: begin
-                    err        = err1 | err2 | err3;                    
-                    valid_out  = 1;
-                    next_state = IDLE;
-                end
-            endcase
-    end
 endmodule
