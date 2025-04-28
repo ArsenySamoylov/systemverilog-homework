f8dfa04c867807a6da50c311b3f1cc73bcfac7bb
ArsenySamoylov <samoylov@ispras.ru>
2025-04-28 14:36:45 +0300

    hw 5 - 6 done

--- 05_fifo_and_single_cycle_cpu/05_06_sqrt_formula_pipe/04_08_formula_1_pipe_aware_fsm.sv
+++ 05_fifo_and_single_cycle_cpu/05_06_sqrt_formula_pipe/04_08_formula_1_pipe_aware_fsm.sv
@@ -1,3 +1,7 @@
+//----------------------------------------------------------------------------
+// Task
+//----------------------------------------------------------------------------
+
 module formula_1_pipe_aware_fsm
 (
     input               clk,
@@ -19,7 +23,132 @@ module formula_1_pipe_aware_fsm
     input               isqrt_y_vld,
     input        [15:0] isqrt_y
 );
-    // NOTHING TO DO HERE
+
+    // Task:
+    //
+    // Implement a module formula_1_pipe_aware_fsm
+    // with a Finite State Machine (FSM)
+    // that drives the inputs and consumes the outputs
+    // of a single pipelined module isqrt.
+    //
+    // The formula_1_pipe_aware_fsm module is supposed to be instantiated
+    // inside the module formula_1_pipe_aware_fsm_top,
+    // together with a single instance of isqrt.
+    //
+    // The resulting structure has to compute the formula
+    // defined in the file formula_1_fn.svh.
+    //
+    // The formula_1_pipe_aware_fsm module
+    // should NOT create any instances of isqrt module,
+    // it should only use the input and output ports connecting
+    // to the instance of isqrt at higher level of the instance hierarchy.
+    //
+    // All the datapath computations except the square root calculation,
+    // should be implemented inside formula_1_pipe_aware_fsm module.
+    // So this module is not a state machine only, it is a combination
+    // of an FSM with a datapath for additions and the intermediate data
+    // registers.
+    //
+    // Note that the module formula_1_pipe_aware_fsm is NOT pipelined itself.
+    // It should be able to accept new arguments a, b and c
+    // arriving at every N+3 clock cycles.
+    //
+    // In order to achieve this latency the FSM is supposed to use the fact
+    // that isqrt is a pipelined module.
+    //
+    // For more details, see the discussion of this problem
+    // in the article by Yuri Panchul published in
+    // FPGA-Systems Magazine :: FSM :: Issue ALFA (state_0)
+    // You can download this issue from https://fpga-systems.ru/fsm#state_0
+
+    // Состояния FSM
+    typedef enum logic [2:0] {
+        IDLE,       
+        SEND_A,     
+        SEND_B,     
+        SEND_C,     
+        COLLECT,    
+        OUTPUT     
+    } state_t;
+
+    state_t state, next_state;
+
+    logic [31:0] sqrt_a, sqrt_b; 
+    logic [1:0] collect_count;   
+
+    always_comb begin
+        next_state = state;
+        isqrt_x_vld = 1'b0;
+        isqrt_x = 32'b0;
+        res_vld = 1'b0;
+
+        case (state)
+            IDLE: begin
+                if (arg_vld) begin
+                    next_state = SEND_A;
+                    isqrt_x_vld = 1'b1;
+                    isqrt_x = a;
+                end
+            end
+
+            SEND_A: begin
+                next_state = SEND_B;
+                isqrt_x_vld = 1'b1;
+                isqrt_x = b;
+            end
+
+            SEND_B: begin
+                next_state = SEND_C;
+                isqrt_x_vld = 1'b1;
+                isqrt_x = c;
+            end
+
+            SEND_C: begin
+                next_state = COLLECT;
+            end
+
+            COLLECT: begin
+                if (isqrt_y_vld && collect_count == 2) begin
+                    next_state = OUTPUT;
+                end
+            end
+
+            OUTPUT: begin
+                res_vld = 1'b1;
+                next_state = IDLE;
+            end
+
+            default: next_state = IDLE;
+        endcase
+    end
+
+    always_ff @(posedge clk) 
+    begin
+        if (rst) begin
+            state <= IDLE;
+            sqrt_a <= 32'b0;
+            sqrt_b <= 32'b0;
+            collect_count <= 2'b0;
+            res <= 32'b0;
+        end 
+        else begin
+            state <= next_state;
+
+            if (isqrt_y_vld) begin
+                case (collect_count)
+                    2'd0: sqrt_a <= {16'b0, isqrt_y}; 
+                    2'd1: sqrt_b <= {16'b0, isqrt_y}; 
+                    2'd2: res <= sqrt_a + sqrt_b + {16'b0, isqrt_y}; 
+                endcase
+                
+                collect_count <= collect_count + 1;
+            end
+
+            if (state == OUTPUT) begin
+                collect_count <= 2'b0;
+            end
+        end
+    end
 
 
 endmodule
--- 05_fifo_and_single_cycle_cpu/05_06_sqrt_formula_pipe/04_10_formula_2_pipe.sv
+++ 05_fifo_and_single_cycle_cpu/05_06_sqrt_formula_pipe/04_10_formula_2_pipe.sv
@@ -16,7 +16,87 @@ module formula_2_pipe
     output [31:0] res
 );
 
-    // NOTHING TO DO HERE
+    // Task:
+    //
+    // Implement a pipelined module formula_2_pipe that computes the result
+    // of the formula defined in the file formula_2_fn.svh.
+    //
+    // The requirements:
+    //
+    // 1. The module formula_2_pipe has to be pipelined.
+    //
+    // It should be able to accept a new set of arguments a, b and c
+    // arriving at every clock cycle.
+    //
+    // It also should be able to produce a new result every clock cycle
+    // with a fixed latency after accepting the arguments.
+    //
+    // 2. Your solution should instantiate exactly 3 instances
+    // of a pipelined isqrt module, which computes the integer square root.
+    //
+    // 3. Your solution should save dynamic power by properly connecting
+    // the valid bits.
+    //
+    // You can read the discussion of this problem
+    // in the article by Yuri Panchul published in
+    // FPGA-Systems Magazine :: FSM :: Issue ALFA (state_0)
+    // You can download this issue from https://fpga-systems.ru/fsm#state_0
+    logic        sqrt_c_vld;
+    logic [15:0] sqrt_c_res;
+    logic        sqrt_bc_vld;
+    logic [15:0] sqrt_bc_res;
 
+    logic [31:0] a_reg [0:7]; 
+    logic [31:0] b_reg [0:3]; 
 
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            for (int i = 0; i < 8; i++) 
+                a_reg[i] <= 0;
+
+            for (int i = 0; i < 4; i++) 
+                b_reg[i] <= 0;
+
+        end else begin
+            if (arg_vld) begin
+                a_reg[0] <= a;
+                b_reg[0] <= b;
+            end
+
+            for (int i = 1; i < 8; i++) 
+                a_reg[i] <= a_reg[i-1];
+
+            for (int i = 1; i < 4; i++) 
+                b_reg[i] <= b_reg[i-1];
+        end
+    end
+
+    isqrt #(.n_pipe_stages(4)) isqrt_c_inst (
+        .clk    (clk),
+        .rst    (rst),
+        .x_vld  (arg_vld),
+        .x      (c),
+        .y_vld  (sqrt_c_vld),
+        .y      (sqrt_c_res)
+    );
+
+    isqrt #(.n_pipe_stages(4)) isqrt_bc_inst (
+        .clk    (clk),
+        .rst    (rst),
+        .x_vld  (sqrt_c_vld),
+        .x      (b_reg[3] + {16'b0, sqrt_c_res}),
+        .y_vld  (sqrt_bc_vld),
+        .y      (sqrt_bc_res)
+    );
+
+    isqrt #(.n_pipe_stages(4)) isqrt_abc_inst (
+        .clk    (clk),
+        .rst    (rst),
+        .x_vld  (sqrt_bc_vld),
+        .x      (a_reg[7] + {16'b0, sqrt_bc_res}),
+        .y_vld  (res_vld),
+        .y      (res[15:0])
+    );
+
+    assign res[31:16] = 16'b0;
 endmodule
